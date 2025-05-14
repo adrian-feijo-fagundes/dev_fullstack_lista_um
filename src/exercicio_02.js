92d7ce53b9cea03e04f92fca3b93181231d70e58
Adrian Feijó Fagundes <adrianfeijofagundes@gmail.com>
2025-05-14 11:32:58 -0300

    Resolvendo exercicio 02

---
+++ src/exercicio_02.js
@@ -0,0 +1,17 @@
+
+
+
+const classifyAge = (age) => {
+    if (age > 59) return "Idoso";
+    if (age > 17) return "Adulto";
+    if (age > 11) return "Adolescente";
+    return "Criança";
+}
+
+export const classifyAgeExample = () => {
+    console.log(
+        "  2. Crie um programa que classifica a idade de uma pessoa em categorias (criança,",
+        "\nadolescente, adulto, idoso) com base no valor fornecido, utilizando uma estrutura de",
+        "\ncontrole if-else."
+    )
+}
