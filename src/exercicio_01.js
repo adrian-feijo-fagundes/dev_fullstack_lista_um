b21045c67f4b1de44fc02c449883b85474a8f283
Adrian Feijó Fagundes <adrianfeijofagundes@gmail.com>
2025-05-14 11:32:45 -0300

    Resolvendo exercicio 01

---
+++ src/exercicio_01.js
@@ -0,0 +1,15 @@
+
+const isEven = (num) => {
+    if (num % 2 == 0) return true;
+    return false;
+}
+
+export const isEvenExample = () => {
+    console.log("1. Escreva um programa que recebe um número inteiro e verifica \n se ele é par ou ímpar utilizando uma estrutura de controle if.")
+    
+    const nums = [1,2,3,4,5,6,7,8]
+    for (let num of nums) {
+        let result = isEven(num) ? "sim" : "não"
+        console.log(` O número ${num} é par? ${result}`)
+    }
+}
